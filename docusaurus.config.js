051ac8ff4c25c4804702c6d1c94f5ee708978a09
sripotturu <54682689+sripotturu@users.noreply.github.com>
2022-11-03 23:09:16 -0400

    Updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'One1fy Docs',
   tagline: 'It\'s Gonna Be Great',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://one1fy.github.io/one1fy-docs/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,9 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'one1fy', // Usually your GitHub org/user name.
   projectName: 'one1fy-docs', // Usually your repo name.
-
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
